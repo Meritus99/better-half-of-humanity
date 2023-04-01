6d4fbc156760943cb81afd0715040f2d4a94a58c
Artem Holodyr <artemmholodyr@gmail.com>
2023-04-01 14:28:24 +0300

    test commit; added to .gitignore

--- coolsite/settings.py
+++ coolsite/settings.py
@@ -27,7 +27,7 @@ env = environ.Env(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = env('DEBUG', default=True)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
